5697118edf835a9ccb6e8e782ce64abe8c11ae76
Jimmy Royer <jroyer@expedia.com>
2016-06-19 17:15:23 -0400

    Added initial data setup + first plot

---
+++ plot1.R
@@ -0,0 +1,18 @@
+source("setup_data.R")
+
+### Configure graphic device
+
+png(filename = "plot1.png",
+    width = 480, height = 480, units = "px", pointsize = 12,
+    bg = "white",  res = NA, type = c("cairo", "cairo-png", "Xlib", "quartz"))
+
+hist(consumptions$Global_active_power, 
+     main = "Global Active Power", 
+     col = "red", 
+     xlab = "Global Active Power (kilowatts)",
+     ylab = "Frequency"
+)
+
+### Flush PNG device to disk
+dev.off()
+
---
+++ setup_data.R
@@ -0,0 +1,49 @@
+
+### General R Configuration
+options(stringsAsFactors=FALSE)
+
+### Utility functions
+
+download_unzip <- function(url, zip_directory, zip_file) {
+  dir.create(file.path(zip_directory), showWarnings = FALSE)
+  ziploc <- paste(zip_directory, zip_file, sep = "/")
+  if (!file.exists(ziploc)) {
+    download.file(url, ziploc, method = "curl")
+    unzip(ziploc, exdir = zip_directory, overwrite = F)
+  }
+}
+
+install_deps <- function(...) {
+  deps <- unlist(list(...))
+  new.packages <- deps[!(deps %in% installed.packages()[,"Package"])]
+  if(length(new.packages)) install.packages(new.packages)
+}
+
+### Dependencies
+
+install_deps("lubridate", "dplyr")
+library(lubridate)
+library(dplyr)
+
+### Getting data
+
+# Download & extract data if not existing in workspace
+zip_file <- "https://d396qusza40orc.cloudfront.net/exdata/data/household_power_consumption.zip"
+download_unzip(zip_file, "data", "household_power_consumptions.zip")
+
+### Reading data
+consumptions <- read.csv(
+  "data/household_power_consumption.txt", 
+  sep = ";",
+  header = TRUE,
+  na.strings = "?"
+)
+
+# Converting columns to proper classes
+consumptions$Date <- dmy(consumptions$Date)
+consumptions$Time <- hms(consumptions$Time)
+
+# Filter the data between 2007-02-01 and 2007-02-02 (included)
+date_from <- ymd("2007-02-01")
+date_to <- ymd("2007-02-02")
+consumptions <- consumptions %>% filter(Date >= date_from & Date <= date_to)
